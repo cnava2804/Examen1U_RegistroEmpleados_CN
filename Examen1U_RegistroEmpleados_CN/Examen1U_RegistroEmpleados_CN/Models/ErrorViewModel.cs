b8aa949a7eaabc2a3b9db5307dfa6503c30dd6ad
cnava2804 <aristides2804@gmail.com>
2021-06-17 17:38:14 -0600

    Inicio del Examen

---
+++ Examen1U_RegistroEmpleados_CN/Examen1U_RegistroEmpleados_CN/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Examen1U_RegistroEmpleados_CN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
